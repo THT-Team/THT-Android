496bdfa17e705146b5ad1f6f9032c4567efa4b3c
kwangddang <kwangddang12@naver.com>
2022-12-19 17:13:44 +0900

    chore: #10 versionCatalog 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,13 +5,22 @@ pluginManagement {
         mavenCentral()
     }
 }
+
+enableFeaturePreview("VERSION_CATALOGS")
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libraries.version.toml"))
+        }
+    }
 }
+
 rootProject.name = "THT"
 include(":app")
 include(":domain")
