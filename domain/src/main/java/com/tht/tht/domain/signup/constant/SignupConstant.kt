6299c32e93a1cb9f60eff1b8da820b8a8dd40d36
wjchoi96 <wjchoi96@naver.com>
2023-01-23 21:41:01 +0900

    feat: Top-5 SignupConstant 추가

---
+++ domain/src/main/java/com/tht/tht/domain/signup/constant/SignupConstant.kt
@@ -0,0 +1,9 @@
+package com.tht.tht.domain.signup.constant
+
+internal object SignupConstant {
+    const val ProfileImageRequireSize = 2
+
+    const val IdealTypeRequireSize = 3
+
+    const val InterestRequireSize = 3
+}
