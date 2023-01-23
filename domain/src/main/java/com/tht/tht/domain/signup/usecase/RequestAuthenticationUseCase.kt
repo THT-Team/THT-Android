f98d06a9b97b08d81e1bc007ca15ac67fbaeb01a
wjchoi96 <wjchoi96@naver.com>
2023-01-23 21:45:21 +0900

    feat: Top-5 RequestAuthenticationUseCase 추가

---
+++ domain/src/main/java/com/tht/tht/domain/signup/usecase/RequestAuthenticationUseCase.kt
@@ -0,0 +1,18 @@
+package com.tht.tht.domain.signup.usecase
+
+import com.tht.tht.domain.signup.repository.SignupRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+
+class RequestAuthenticationUseCase(
+    private val repository: SignupRepository,
+    private val dispatcher: CoroutineDispatcher
+) {
+    suspend operator fun invoke(phone: String): Result<Boolean> {
+        return kotlin.runCatching {
+            withContext(dispatcher) {
+                repository.requestAuthentication(phone)
+            }
+        }
+    }
+}
