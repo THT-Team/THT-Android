88aafc88176477417216e659984679fa0f69a420
wjchoi96 <wjchoi96@naver.com>
2023-02-11 16:47:45 +0900

    modify: TOP-9 TermsModel 수정
     - TermsModel.TermsContent 정의
     - TermsModel의 content를 List<TermsContent>로 변경
     - 변경에 따른 테스트 케이스 수정

--- domain/src/main/java/com/tht/tht/domain/signup/model/TermsModel.kt
+++ domain/src/main/java/com/tht/tht/domain/signup/model/TermsModel.kt
@@ -2,6 +2,11 @@ package com.tht.tht.domain.signup.model
 
 data class TermsModel(
     val title: String,
-    val content: String,
+    val content: List<TermsContent>,
     val require: Boolean
-)
+) {
+    data class TermsContent(
+        val title: String,
+        val content: String
+    )
+}
