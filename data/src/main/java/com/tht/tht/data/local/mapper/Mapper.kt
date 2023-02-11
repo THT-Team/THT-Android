f9501e3b29b16df2a3fe98f38218f188fcde64da
wjchoi96 <wjchoi96@naver.com>
2023-02-11 19:30:04 +0900

    feat: TOP-9 SignupUserEntity 정의 및 Mapper 함수 추가

--- data/src/main/java/com/tht/tht/data/local/mapper/Mapper.kt
+++ data/src/main/java/com/tht/tht/data/local/mapper/Mapper.kt
@@ -1,6 +1,8 @@
 package com.tht.tht.data.local.mapper
 
+import com.tht.tht.data.local.entity.SignupUserEntity
 import com.tht.tht.data.local.entity.TermsEntity
+import com.tht.tht.domain.signup.model.SignupUserModel
 import com.tht.tht.domain.signup.model.TermsModel
 
 // 확장 함수
@@ -14,5 +16,43 @@ fun TermsEntity.Body.toModel(): TermsModel {
             )
         },
         require = require
+    )
+}
+
+fun SignupUserEntity.toModel(): SignupUserModel {
+    return SignupUserModel(
+        phone = phone,
+        termsAgreement = termsAgreement,
+        nickname = nickname,
+        email = email,
+        gender = gender,
+        birth = birth,
+        interestKeys = interestKeys,
+        lat = lat,
+        lng = lng,
+        address = address,
+        preferredGender = preferredGender,
+        profileImgUrl = profileImgUrl,
+        introduce = introduce,
+        idealTypeKeys = idealTypeKeys
+    )
+}
+
+fun SignupUserModel.toEntity(): SignupUserEntity {
+    return SignupUserEntity(
+        phone = phone,
+        termsAgreement = termsAgreement,
+        nickname = nickname,
+        email = email,
+        gender = gender,
+        birth = birth,
+        interestKeys = interestKeys,
+        lat = lat,
+        lng = lng,
+        address = address,
+        preferredGender = preferredGender,
+        profileImgUrl = profileImgUrl,
+        introduce = introduce,
+        idealTypeKeys = idealTypeKeys
     )
 }
