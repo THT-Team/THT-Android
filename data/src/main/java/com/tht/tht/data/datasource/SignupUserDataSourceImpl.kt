1996123597c15e5168fa2eab5887ecefe534e2e7
wjchoi96 <wjchoi96@naver.com>
2023-02-11 19:31:10 +0900

    feat: TOP-9 SignupUserDataSource, 구현체 작성
     - SharedPrererneces를 주입받아 회원가입 프로세스를 진행중인 유저 데이터를 local에서 관리

---
+++ data/src/main/java/com/tht/tht/data/datasource/SignupUserDataSourceImpl.kt
@@ -0,0 +1,52 @@
+package com.tht.tht.data.datasource
+
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.tht.tht.data.di.IODispatcher
+import com.tht.tht.data.local.entity.SignupUserEntity
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class SignupUserDataSourceImpl @Inject constructor(
+    private val sp: SharedPreferences,
+    @IODispatcher private val dispatcher: CoroutineDispatcher
+) : SignupUserDataSource {
+    companion object {
+        private const val SIGNUP_USER_KEY = "signup_user_key"
+    }
+
+    private var signupUserMap: HashMap<String, SignupUserEntity>
+        get() {
+            val typeToken = object : TypeToken<HashMap<String, SignupUserEntity>>() {}.type
+            return Gson().fromJson(sp.getString(SIGNUP_USER_KEY, ""), typeToken)
+        }
+        set(value) {
+            sp.edit { putString(SIGNUP_USER_KEY, Gson().toJson(value)) }
+        }
+
+    override suspend fun saveSignupUser(phone: String, user: SignupUserEntity): SignupUserEntity {
+        return withContext(dispatcher){
+            signupUserMap = signupUserMap.apply { this[phone] = user }
+            requireNotNull(signupUserMap[phone])
+        }
+    }
+
+    override suspend fun fetchSignupUser(phone: String): SignupUserEntity? {
+        return withContext(dispatcher) {
+            signupUserMap[phone]
+        }
+    }
+
+    override suspend fun removeSignupUser(phone: String): Boolean {
+        return withContext(dispatcher) {
+            val removeRes: Boolean
+            signupUserMap = signupUserMap.apply {
+                removeRes = remove(phone) != null
+            }
+            removeRes
+        }
+    }
+}
