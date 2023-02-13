9e69b2eb88bfd095f51fb58e2fd240d72fc2b04a
wjchoi96 <wjchoi96@naver.com>
2023-02-13 15:59:04 +0900

    modify: TOP-9 SignupUserDataSource 구현체 수정
     - SignupUser를 Local에 직접적으로 관리하는 역할과, Local 혹은 Remote에서 SignupUser를 호출하는 책임을 분리하기위해 SignupUserDao를 정의, SignupUser를 Local에서 관리하는 코드를 이동시킴
     - SignupUserDataSourceImpl에서는 Local에서 SignupUser를 관리하는 SignupUserDao를 올바른 Dispatcher에서 호출하여 결과를 리턴하는 역할을 수행

--- data/src/main/java/com/tht/tht/data/local/datasource/SignupUserDataSourceImpl.kt
+++ data/src/main/java/com/tht/tht/data/local/datasource/SignupUserDataSourceImpl.kt
@@ -5,48 +5,31 @@ import androidx.core.content.edit
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.tht.tht.data.di.IODispatcher
+import com.tht.tht.data.local.dao.SignupUserDao
 import com.tht.tht.data.local.entity.SignupUserEntity
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class SignupUserDataSourceImpl @Inject constructor(
-    private val sp: SharedPreferences,
+    private val signupUserDao: SignupUserDao,
     @IODispatcher private val dispatcher: CoroutineDispatcher
 ) : SignupUserDataSource {
-    companion object {
-        private const val SIGNUP_USER_KEY = "signup_user_key"
-    }
-
-    private var signupUserMap: HashMap<String, SignupUserEntity>
-        get() {
-            val typeToken = object : TypeToken<HashMap<String, SignupUserEntity>>() {}.type
-            return Gson().fromJson(sp.getString(SIGNUP_USER_KEY, ""), typeToken) ?: HashMap()
-        }
-        set(value) {
-            sp.edit { putString(SIGNUP_USER_KEY, Gson().toJson(value)) }
-        }
-
     override suspend fun saveSignupUser(phone: String, user: SignupUserEntity): SignupUserEntity {
         return withContext(dispatcher){
-            signupUserMap = signupUserMap.apply { this[phone] = user }
-            requireNotNull(signupUserMap[phone])
+            signupUserDao.saveSignupUser(phone, user)
         }
     }
 
     override suspend fun fetchSignupUser(phone: String): SignupUserEntity? {
         return withContext(dispatcher) {
-            signupUserMap[phone]
+            signupUserDao.fetchSignupUser(phone)
         }
     }
 
     override suspend fun removeSignupUser(phone: String): Boolean {
         return withContext(dispatcher) {
-            val removeRes: Boolean
-            signupUserMap = signupUserMap.apply {
-                removeRes = remove(phone) != null
-            }
-            removeRes
+            signupUserDao.removeSignupUser(phone)
         }
     }
 }
