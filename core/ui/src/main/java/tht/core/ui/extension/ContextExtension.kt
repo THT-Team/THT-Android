f3124d25adc393b7f1c4da36266ebed574b1977e
wjchoi96 <wjchoi96@naver.com>
2023-02-21 23:52:12 +0900

    add: TOP-17 ContextExtension.kt 파일을 core-ui 모듈에 추가

---
+++ core/ui/src/main/java/tht/core/ui/extension/ContextExtension.kt
@@ -0,0 +1,11 @@
+package tht.core.ui.extension
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+
+
+fun Context.showToast(message: String) {
+    Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+}
