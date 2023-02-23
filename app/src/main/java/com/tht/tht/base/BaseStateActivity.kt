d7b401a8e9fcecb473632af2d3f17125634f8ac0
bn-tw2020 <skaxodn97@gmail.com>
2023-02-23 20:29:46 +0900

    fix: TOP-30 lint check

--- app/src/main/java/com/tht/tht/base/BaseStateActivity.kt
+++ app/src/main/java/com/tht/tht/base/BaseStateActivity.kt
@@ -2,4 +2,4 @@ package com.tht.tht.base
 
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseStateActivity<VM: BaseStateViewModel<*, *>, VB: ViewBinding>: BaseActivity<VM, VB>()
+abstract class BaseStateActivity<VM : BaseStateViewModel<*, *>, VB : ViewBinding> : BaseActivity<VM, VB>()
