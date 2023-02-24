785726a28ac38ed4c65e0f771c65b80e0a777974
wjchoi96 <wjchoi96@naver.com>
2023-02-24 19:49:07 +0900

    refactor: TOP-18 PhoneAuthViewModel UiState, SideEffect 방출 코드 수정
     - 의도치 않게 Job을 리턴하는 형태의 메소드들을 수정
     - BaseStateViewModel에 정의된 postSideEffect, setUiState를 적용해 코드가 개선되는 부분이라면 적용하여 개선

--- feature/signin/src/main/java/tht/feature/signin/PhoneAuthViewModel.kt
+++ feature/signin/src/main/java/tht/feature/signin/PhoneAuthViewModel.kt
@@ -22,48 +22,50 @@ class PhoneAuthViewModel @Inject constructor(
     private val _dataLoading = MutableStateFlow(false)
     val dataLoading = _dataLoading.asStateFlow()
 
-    fun textInputEvent(text: String?) = viewModelScope.launch {
+    fun textInputEvent(text: String?) {
         if (text.isNullOrBlank()) {
-            _uiStateFlow.emit(PhoneAuthUiState.InputPhoneNumEmpty)
-            return@launch
+            setUiState(PhoneAuthUiState.InputPhoneNumEmpty)
+            return
         }
         when (checkPhoneValidation(text)) {
-            true -> _uiStateFlow.emit(PhoneAuthUiState.InputPhoneNumCorrect)
-            else -> _uiStateFlow.emit(PhoneAuthUiState.InputPhoneNumError)
+            true -> setUiState(PhoneAuthUiState.InputPhoneNumCorrect)
+            else -> setUiState(PhoneAuthUiState.InputPhoneNumError)
         }
-    }
 
+    }
     private fun checkPhoneValidation(phone: String): Boolean {
         val phoneNumPattern = "^01([0|16789])(\\d{4})(\\d{4})$"
         return Pattern.matches(phoneNumPattern, phone)
     }
 
-    fun backgroundTouchEvent() = viewModelScope.launch {
-        _sideEffectFlow.emit(PhoneAuthSideEffect.KeyboardVisible(false))
+    fun backgroundTouchEvent() {
+        postSideEffect(PhoneAuthSideEffect.KeyboardVisible(false))
     }
 
-    fun backEvent() = viewModelScope.launch {
-        _sideEffectFlow.emit(PhoneAuthSideEffect.Back)
+    fun backEvent() {
+        postSideEffect(PhoneAuthSideEffect.Back)
     }
 
-    fun authEvent(phone: String?) = viewModelScope.launch {
-        if (phone.isNullOrBlank() || !checkPhoneValidation(phone)) {
-            _uiStateFlow.emit(PhoneAuthUiState.InputPhoneNumError)
-            return@launch
-        }
-        _dataLoading.value = true
-        requestAuthenticationUseCase(phone)
-            .onSuccess {
-                when (it) {
-                    true -> _sideEffectFlow.emit(PhoneAuthSideEffect.NavigateVerifyView(phone))
-                    else -> _sideEffectFlow.emit(PhoneAuthSideEffect.ShowToast("fail send auth"))
-                }
-            }.onFailure {
-                _sideEffectFlow.emit(PhoneAuthSideEffect.ShowToast(it.message ?: "$it"))
-            }.also {
-                _sideEffectFlow.emit(PhoneAuthSideEffect.KeyboardVisible(false))
-                _dataLoading.value = false
+    fun authEvent(phone: String?) {
+        viewModelScope.launch {
+            if (phone.isNullOrBlank() || !checkPhoneValidation(phone)) {
+                setUiState(PhoneAuthUiState.InputPhoneNumError)
+                return@launch
             }
+            _dataLoading.value = true
+            requestAuthenticationUseCase(phone)
+                .onSuccess {
+                    when (it) {
+                        true -> _sideEffectFlow.emit(PhoneAuthSideEffect.NavigateVerifyView(phone))
+                        else -> _sideEffectFlow.emit(PhoneAuthSideEffect.ShowToast("fail send auth"))
+                    }
+                }.onFailure {
+                    _sideEffectFlow.emit(PhoneAuthSideEffect.ShowToast(it.message ?: "$it"))
+                }.also {
+                    _sideEffectFlow.emit(PhoneAuthSideEffect.KeyboardVisible(false))
+                    _dataLoading.value = false
+                }
+        }
     }
 
     sealed class PhoneAuthUiState : UiState {
