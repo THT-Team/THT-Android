411eefab9dc90c0fad1a18a18f40215ee7c003ac
wjchoi96 <wjchoi96@naver.com>
2023-02-24 19:29:44 +0900

    rename: TOP-18 State interface를 UiState로 네이밍 변경

--- core/ui/src/main/java/tht/core/ui/base/BaseStateViewModel.kt
+++ core/ui/src/main/java/tht/core/ui/base/BaseStateViewModel.kt
@@ -7,26 +7,26 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-abstract class BaseStateViewModel<S : State, SE : SideEffect> : BaseViewModel() {
+abstract class BaseStateViewModel<S : UiState, SE : SideEffect> : BaseViewModel() {
 
-    protected abstract val _stateFlow: MutableStateFlow<S>
-    val stateFlow: StateFlow<S>
-        get() = _stateFlow
+    protected abstract val _uiStateFlow: MutableStateFlow<S>
+    val uiStateFlow: StateFlow<S>
+        get() = _uiStateFlow
 
-    protected abstract val _sideEffectFlow: MutableSharedFlow<SE>
+    protected val _sideEffectFlow: MutableSharedFlow<SE> = MutableSharedFlow()
     val sideEffectFlow: SharedFlow<SE>
         get() = _sideEffectFlow
 
-    protected inline fun <reified S : State> withState(block: (S) -> Unit): Boolean {
-        if (stateFlow.value is S) {
-            block(stateFlow.value as S)
+    protected inline fun <reified S : UiState> withUiState(block: (S) -> Unit): Boolean {
+        if (uiStateFlow.value is S) {
+            block(uiStateFlow.value as S)
             return true
         }
         return false
     }
 
-    protected fun setState(state: S) {
-        _stateFlow.value = state
+    protected fun setUiState(state: S) {
+        _uiStateFlow.value = state
     }
 
     protected fun postSideEffect(sideEffect: SE) {
