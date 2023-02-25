900a1f0c5c4c71d71260a1e68441b8d9891d094c
bn-tw2020 <skaxodn97@gmail.com>
2023-02-25 11:27:47 +0900

    chore: TOP-32 카카오 gradle 설정

--- app/src/main/java/com/tht/tht/TopicHotTodayApp.kt
+++ app/src/main/java/com/tht/tht/TopicHotTodayApp.kt
@@ -1,7 +1,18 @@
 package com.tht.tht
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TopicHotTodayApp : Application()
+class TopicHotTodayApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initKakaoSDK()
+    }
+
+    private fun initKakaoSDK() {
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+    }
+}
