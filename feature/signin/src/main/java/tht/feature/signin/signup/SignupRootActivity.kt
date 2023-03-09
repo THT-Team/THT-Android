07fbd6f7c684db1ca9aed8fd644764de9895a03d
kwangddang <kwangddang12@naver.com>
2023-03-09 23:52:43 +0900

    feat: TOP-16 SignupRootActivity 생성

---
+++ feature/signin/src/main/java/tht/feature/signin/signup/SignupRootActivity.kt
@@ -0,0 +1,11 @@
+package tht.feature.signin.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupRootActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_root)
+    }
+}
