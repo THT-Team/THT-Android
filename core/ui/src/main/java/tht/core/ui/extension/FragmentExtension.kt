703fd39a7565f6a4abb920ac86a89298ba51293c
kwangddang <kwangddang12@naver.com>
2023-03-16 16:46:21 +0900

    feat: TOP-16 FragmentExtension 생성

--- core/ui/src/main/java/tht/core/ui/extension/FragmentExtension.kt
+++ core/ui/src/main/java/tht/core/ui/extension/FragmentExtension.kt
@@ -1,6 +1,5 @@
 package tht.core.ui.extension
 
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -8,8 +7,8 @@ import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-fun AppCompatActivity.repeatOnStarted(block: suspend CoroutineScope.() -> Unit) {
-    lifecycleScope.launch {
+fun Fragment.repeatOnStarted(block: suspend CoroutineScope.() -> Unit) {
+    viewLifecycleOwner.lifecycleScope.launch {
         lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED, block)
     }
 }
