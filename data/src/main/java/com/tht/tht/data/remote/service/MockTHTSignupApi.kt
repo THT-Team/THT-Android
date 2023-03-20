27cd863a23b3e817a4ce047c33c962686c2e5ec9
wjchoi96 <wjchoi96@naver.com>
2023-03-21 01:36:07 +0900

    feat: TOP-35 MockTHTSignupApi 구현
    - API 구현 이전에 앱 사이드 구현을 위한 MockApiService 구현

---
+++ data/src/main/java/com/tht/tht/data/remote/service/MockTHTSignupApi.kt
@@ -0,0 +1,48 @@
+package com.tht.tht.data.remote.service
+
+import com.tht.tht.data.remote.response.base.BaseResponse
+import com.tht.tht.data.remote.response.base.SuccessResponse
+import com.tht.tht.data.remote.response.base.ThtResponse
+import com.tht.tht.data.remote.response.ideal.IdealTypeResponse
+import com.tht.tht.data.remote.response.interests.InterestTypeResponse
+import com.tht.tht.data.remote.response.signup.SignupResponse
+import com.tht.tht.domain.signup.model.SignupUserModel
+
+class MockTHTSignupApi : THTSignupApi {
+    override suspend fun requestAuthenticationNumber(phone: String): ThtResponse<Boolean> {
+        return BaseResponse.Success(
+            statusCode = 200,
+            response = SuccessResponse(true)
+        )
+    }
+
+    override suspend fun requestVerify(phone: String, authNumber: String): ThtResponse<Boolean> {
+        return BaseResponse.Success(
+            statusCode = 200,
+            response = SuccessResponse(true)
+        )
+    }
+
+    override suspend fun fetchIdealType(): ThtResponse<List<IdealTypeResponse>> {
+        val idealTypes = listOf(IdealTypeResponse("name", "code", 0))
+        return BaseResponse.Success(
+            statusCode = 200,
+            response = SuccessResponse(idealTypes)
+        )
+    }
+
+    override suspend fun fetchInterestsType(): ThtResponse<List<InterestTypeResponse>> {
+        val interestType = listOf(InterestTypeResponse("name", "code", 0))
+        return BaseResponse.Success(
+            statusCode = 200,
+            response = SuccessResponse(interestType)
+        )
+    }
+
+    override suspend fun requestSignup(user: SignupUserModel): ThtResponse<SignupResponse> {
+        return BaseResponse.Success(
+            statusCode = 200,
+            response = SuccessResponse(SignupResponse("userId"))
+        )
+    }
+}
