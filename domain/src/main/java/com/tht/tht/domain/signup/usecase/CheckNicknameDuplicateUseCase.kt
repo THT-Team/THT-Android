8d5b768531cc3b4995060c54ed54ae5a4f853829
wjchoi96 <wjchoi96@naver.com>
2023-03-22 15:46:41 +0900

    feat: TOP-31 CheckNicknameDuplicateUseCase에 SignupRepository적용

--- domain/src/main/java/com/tht/tht/domain/signup/usecase/CheckNicknameDuplicateUseCase.kt
+++ domain/src/main/java/com/tht/tht/domain/signup/usecase/CheckNicknameDuplicateUseCase.kt
@@ -2,7 +2,7 @@ package com.tht.tht.domain.signup.usecase
 
 import com.tht.tht.domain.signup.repository.SignupRepository
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 /**
  *  중복 검사 API 구현 이전, 단순한 테스트용
@@ -14,13 +14,10 @@ class CheckNicknameDuplicateUseCase(
 ) {
 
     suspend operator fun invoke(nickname: String): Result<Boolean> {
-        delay(100)
-        val rValue = (1 until 4).random()
-        println("Random value => $rValue")
-        return when (rValue % 3) {
-            0 -> Result.success(true)
-            1 -> Result.success(false)
-            else -> Result.failure(Throwable("test"))
+        return withContext(dispatcher) {
+            kotlin.runCatching {
+                repository.checkNicknameDuplicate(nickname)
+            }
         }
     }
 }
