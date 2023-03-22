f4d967631ef8ce0fe1cddabbfa67207c05045eb1
wjchoi96 <wjchoi96@naver.com>
2023-03-23 00:37:40 +0900

    feat: TOP-36 THTApiConstant에 번호인증 url 갱신

--- data/src/main/java/com/tht/tht/data/constant/THTApiConstant.kt
+++ data/src/main/java/com/tht/tht/data/constant/THTApiConstant.kt
@@ -4,8 +4,7 @@ object THTApiConstant {
     const val BASE_URL = "http://tht-talk.store/"
 
     object Signup {
-        const val AUTHENTICATION_NUM = "auth"
-        const val VERIFY = "verify"
+        const val AUTHENTICATION_NUM = "user/certification/phone-number"
 
         const val INTERESTS = "ideal-types"
         const val IDEAL_TYPE = "ideal-types"
