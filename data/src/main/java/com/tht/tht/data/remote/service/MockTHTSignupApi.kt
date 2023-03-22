1590c0dfb8bb24c5e6ab360e641a6c5bf04eed72
wjchoi96 <wjchoi96@naver.com>
2023-03-22 15:43:54 +0900

    feat: TOP-31 MockSignupApiService에 테스트를 위한 실패 케이스 추가, delay 추가

--- data/src/main/java/com/tht/tht/data/remote/service/MockTHTSignupApi.kt
+++ data/src/main/java/com/tht/tht/data/remote/service/MockTHTSignupApi.kt
@@ -7,9 +7,11 @@ import com.tht.tht.data.remote.response.ideal.IdealTypeResponse
 import com.tht.tht.data.remote.response.interests.InterestTypeResponse
 import com.tht.tht.data.remote.response.signup.SignupResponse
 import com.tht.tht.domain.signup.model.SignupUserModel
+import kotlinx.coroutines.delay
 
 class MockTHTSignupApi : THTSignupApi {
     override suspend fun requestAuthenticationNumber(phone: String): ThtResponse<Boolean> {
+        delay(100)
         return BaseResponse.Success(
             statusCode = 200,
             response = SuccessResponse(true)
@@ -17,9 +19,16 @@ class MockTHTSignupApi : THTSignupApi {
     }
 
     override suspend fun requestVerify(phone: String, authNumber: String): ThtResponse<Boolean> {
+        delay(100)
+        val res = when (authNumber.contains('0')) {
+            true -> false
+            else -> true
+        }
         return BaseResponse.Success(
             statusCode = 200,
-            response = SuccessResponse(true)
+            response = SuccessResponse(res)
+        )
+    }
 
     override suspend fun checkNicknameDuplicate(nickname: String): ThtResponse<Boolean> {
         delay(100)
@@ -35,6 +44,7 @@ class MockTHTSignupApi : THTSignupApi {
     }
 
     override suspend fun fetchIdealType(): ThtResponse<List<IdealTypeResponse>> {
+        delay(100)
         val idealTypes = listOf(IdealTypeResponse("name", "code", 0))
         return BaseResponse.Success(
             statusCode = 200,
@@ -43,6 +53,7 @@ class MockTHTSignupApi : THTSignupApi {
     }
 
     override suspend fun fetchInterestsType(): ThtResponse<List<InterestTypeResponse>> {
+        delay(100)
         val interestType = listOf(InterestTypeResponse("name", "code", 0))
         return BaseResponse.Success(
             statusCode = 200,
@@ -51,6 +62,7 @@ class MockTHTSignupApi : THTSignupApi {
     }
 
     override suspend fun requestSignup(user: SignupUserModel): ThtResponse<SignupResponse> {
+        delay(100)
         return BaseResponse.Success(
             statusCode = 200,
             response = SuccessResponse(SignupResponse("userId"))
