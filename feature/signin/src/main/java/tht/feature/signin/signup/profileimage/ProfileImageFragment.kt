5b1da36e73db357a00903c247726cd060d5223cf
wjchoi96 <wjchoi96@naver.com>
2023-04-13 00:13:16 +0900

    design: TOP-24 ProfileImageFragment UI 구현

--- feature/signin/src/main/java/tht/feature/signin/signup/profileimage/ProfileImageFragment.kt
+++ feature/signin/src/main/java/tht/feature/signin/signup/profileimage/ProfileImageFragment.kt
@@ -1,5 +1,8 @@
 package tht.feature.signin.signup.profileimage
 
+import android.os.Bundle
+import android.text.InputFilter
+import android.view.View
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -8,6 +11,8 @@ import tht.core.ui.extension.repeatOnStarted
 import tht.feature.signin.databinding.FragmentProfileImageBinding
 import tht.feature.signin.signup.SignupRootBaseFragment
 import tht.feature.signin.signup.SignupRootViewModel
+import tht.feature.signin.signup.nickname.NicknameViewModel
+import tht.feature.signin.util.StringUtil
 
 @AndroidEntryPoint
 class ProfileImageFragment : SignupRootBaseFragment<ProfileImageViewModel, FragmentProfileImageBinding>() {
@@ -16,12 +21,21 @@ class ProfileImageFragment : SignupRootBaseFragment<ProfileImageViewModel, Fragm
 
     override val viewModel by viewModels<ProfileImageViewModel>()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
     override fun setProgress() {
         rootViewModel.progressEvent(SignupRootViewModel.Step.PROFILE_IMAGE)
     }
 
     override fun setListener() {
         binding.btnNext.setOnClickListener { rootViewModel.nextEvent(SignupRootViewModel.Step.PROFILE_IMAGE) }
+    }
+
+    private fun initView() {
+        StringUtil.setWhiteTextColor(binding.tvProfileImageTitle, 0 until 2)
     }
 
     override fun observeData() {
