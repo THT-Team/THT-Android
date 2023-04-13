bb160a9b7a2996a3d07ee3d4b93eb9691ef75571
wjchoi96 <wjchoi96@naver.com>
2023-04-13 22:26:15 +0900

    feat: TOP-24 ProfileImage화면 이미지 업로드를 제외한 구현 완료

--- feature/signin/src/main/java/tht/feature/signin/signup/profileimage/ProfileImageFragment.kt
+++ feature/signin/src/main/java/tht/feature/signin/signup/profileimage/ProfileImageFragment.kt
@@ -1,8 +1,11 @@
 package tht.feature.signin.signup.profileimage
 
+import android.net.Uri
 import android.os.Bundle
-import android.text.InputFilter
 import android.view.View
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -11,7 +14,6 @@ import tht.core.ui.extension.repeatOnStarted
 import tht.feature.signin.databinding.FragmentProfileImageBinding
 import tht.feature.signin.signup.SignupRootBaseFragment
 import tht.feature.signin.signup.SignupRootViewModel
-import tht.feature.signin.signup.nickname.NicknameViewModel
 import tht.feature.signin.util.StringUtil
 
 @AndroidEntryPoint
@@ -20,6 +22,17 @@ class ProfileImageFragment : SignupRootBaseFragment<ProfileImageViewModel, Fragm
     override val binding: FragmentProfileImageBinding by viewBinding(FragmentProfileImageBinding::inflate)
 
     override val viewModel by viewModels<ProfileImageViewModel>()
+
+    private val imageSelectCallback = ImageSelectCallbackWrapper<Uri?>()
+
+    private val photoPicker = registerForActivityResult(ActivityResultContracts.PickVisualMedia(), imageSelectCallback)
+    private val imageViews by lazy {
+        listOf(
+            binding.ivImageAdd1,
+            binding.ivImageAdd2,
+            binding.ivImageAdd3
+        )
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -31,7 +44,13 @@ class ProfileImageFragment : SignupRootBaseFragment<ProfileImageViewModel, Fragm
     }
 
     override fun setListener() {
-        binding.btnNext.setOnClickListener { rootViewModel.nextEvent(SignupRootViewModel.Step.PROFILE_IMAGE) }
+        imageViews.forEachIndexed { index, imageView ->
+            imageView.setOnClickListener { viewModel.imageClickEvent(index) }
+        }
+
+        binding.btnNext.setOnClickListener {
+            viewModel.nextEvent()
+        }
     }
 
     private fun initView() {
@@ -40,18 +59,50 @@ class ProfileImageFragment : SignupRootBaseFragment<ProfileImageViewModel, Fragm
 
     override fun observeData() {
         repeatOnStarted {
-
             launch {
                 viewModel.uiStateFlow.collect {
+                    when (it) {
+                        is ProfileImageUiState.Empty -> {
+                            binding.btnNext.isEnabled = false
+                        }
+                        is ProfileImageUiState.Accept -> {
+                            binding.btnNext.isEnabled = true
+                        }
+                    }
                 }
             }
 
             launch {
                 viewModel.sideEffectFlow.collect {
+                    when (it) {
+                        is ProfileImageSideEffect.RequestImageFromGallery -> {
+                            imageSelectCallback.callback = ActivityResultCallback  { uri ->
+                                if (uri != null) {
+                                    viewModel.imageSelectEvent(uri.toString(), it.idx)
+                                }
+                            }
+                            photoPicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                        }
+
+                        is ProfileImageSideEffect.NavigateNextView ->
+                            rootViewModel.nextEvent(SignupRootViewModel.Step.PROFILE_IMAGE)
+                    }
+                }
+            }
+
+            launch {
+                viewModel.imageUrlList.collect {
+                    it.forEachIndexed { index, url ->
+                        if (url.isBlank()) return@forEachIndexed
+                        if (index !in imageViews.indices) return@collect
+                        imageViews[index].setImageURI(Uri.parse(url))
+                    }
                 }
             }
         }
     }
+
+
     companion object {
 
         val TAG = ProfileImageFragment::class.simpleName.toString()
--- feature/signin/src/main/java/tht/feature/signin/signup/profileimage/ProfileImageViewModel.kt
+++ feature/signin/src/main/java/tht/feature/signin/signup/profileimage/ProfileImageViewModel.kt
@@ -4,6 +4,7 @@ import com.tht.tht.domain.signup.usecase.FetchSignupUserUseCase
 import com.tht.tht.domain.signup.usecase.PatchSignupProfileImagesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import tht.core.ui.base.BaseStateViewModel
 import tht.core.ui.base.SideEffect
 import tht.core.ui.base.UiState
@@ -12,14 +13,53 @@ import javax.inject.Inject
 @HiltViewModel
 class ProfileImageViewModel @Inject constructor(
     private val fetchSignupUserUseCase: FetchSignupUserUseCase,
-    private val patchSignupProfileImagesUseCase: PatchSignupProfileImagesUseCase
-) : BaseStateViewModel<ProfileImageViewModel.ProfileImageUiState, ProfileImageViewModel.ProfileImageSideEffect>() {
+    private val patchSignupProfileImagesUseCase: PatchSignupProfileImagesUseCase,
+) : BaseStateViewModel<ProfileImageUiState, ProfileImageSideEffect>() {
 
     override val _uiStateFlow: MutableStateFlow<ProfileImageUiState> =
         MutableStateFlow(ProfileImageUiState.Empty)
 
-    sealed class ProfileImageUiState : UiState {
-        object Empty : ProfileImageUiState()
+    private val _imageUrlList = MutableStateFlow(Array(IMAGE_MAX_SIZE){ "" })
+    val imageUrlList = _imageUrlList.asStateFlow()
+
+    fun imageClickEvent(idx: Int) {
+        postSideEffect(ProfileImageSideEffect.RequestImageFromGallery(idx))
     }
-    sealed class ProfileImageSideEffect : SideEffect
+
+    fun imageSelectEvent(url: String, idx: Int) {
+        val urlList = _imageUrlList.value
+        urlList[idx] = url
+        _imageUrlList.value = urlList.copyOf()
+
+        var inputCount = 0
+        for (u in urlList) {
+            if (u.isNotBlank()) inputCount++
+        }
+        _uiStateFlow.value = when (inputCount >= IMAGE_REQUIRE_SIZE) {
+            true -> ProfileImageUiState.Accept
+            else -> ProfileImageUiState.Empty
+        }
+    }
+
+    fun nextEvent() {
+        postSideEffect(ProfileImageSideEffect.NavigateNextView)
+    }
+
+    companion object {
+        private const val IMAGE_MAX_SIZE = 3
+        private const val IMAGE_REQUIRE_SIZE = 2
+    }
+
+}
+
+sealed class ProfileImageUiState : UiState {
+    object Empty : ProfileImageUiState()
+    object Accept : ProfileImageUiState()
+}
+sealed class ProfileImageSideEffect : SideEffect {
+    data class RequestImageFromGallery(
+        val idx: Int
+    ) : ProfileImageSideEffect()
+
+    object NavigateNextView : ProfileImageSideEffect()
 }
