802c818d220fd47a230a9f5a3dc8fb2543a16c77
kwangddang <kwangddang12@naver.com>
2023-04-20 15:57:07 +0900

    refactor: TOP-28 LocationService 불필요한 로직 제거

--- data/src/main/java/com/tht/tht/data/remote/service/LocationServiceImpl.kt
+++ data/src/main/java/com/tht/tht/data/remote/service/LocationServiceImpl.kt
@@ -8,7 +8,6 @@ import android.location.LocationManager
 import androidx.core.content.ContextCompat
 import com.tht.tht.data.remote.response.location.LocationResponse
 import dagger.hilt.android.qualifiers.ApplicationContext
-import java.io.IOException
 import java.util.*
 import javax.inject.Inject
 
@@ -44,18 +43,12 @@ class LocationServiceImpl @Inject constructor(
     private fun getAddress(lat: Double, lng: Double): String {
         val geocoder = Geocoder(context, Locale.KOREA)
         val location = geocoder.getFromLocation(lat, lng, 1)
-        if (location == null || location.size == 0)
-            throw IOException("Can not find location")
-
         return location[0].getAddressLine(0)
     }
 
     private fun getCoordinate(locationName: String): Pair<Double, Double> {
         val geocoder = Geocoder(context, Locale.KOREA)
         val location = geocoder.getFromLocationName(locationName, 1)
-        if (location == null || location.size == 0)
-            throw IOException("Can not find location")
-
         return Pair(location[0].latitude, location[0].longitude)
     }
 }
