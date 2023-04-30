d67332ff608e66c5e4e2435cc59238b50f850124
wjchoi96 <wjchoi96@naver.com>
2023-04-30 21:49:10 +0900

    feat:TOP-25 InterestViewModel 구현

--- feature/signin/src/main/java/tht/feature/signin/signup/interest/InterestViewModel.kt
+++ feature/signin/src/main/java/tht/feature/signin/signup/interest/InterestViewModel.kt
@@ -1,25 +1,161 @@
 package tht.feature.signin.signup.interest
 
+import androidx.lifecycle.viewModelScope
+import com.tht.tht.domain.signup.model.InterestModel
+import com.tht.tht.domain.signup.usecase.FetchInterestUseCase
 import com.tht.tht.domain.signup.usecase.FetchSignupUserUseCase
 import com.tht.tht.domain.signup.usecase.PatchSignupInterestUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import tht.core.ui.base.BaseStateViewModel
 import tht.core.ui.base.SideEffect
 import tht.core.ui.base.UiState
+import tht.feature.signin.StringProvider
 import javax.inject.Inject
 
 @HiltViewModel
 class InterestViewModel @Inject constructor(
     private val fetchSignupUserUseCase: FetchSignupUserUseCase,
-    private val patchSignupInterestUseCase: PatchSignupInterestUseCase
-) : BaseStateViewModel<InterestViewModel.InterestUiState, InterestViewModel.InterestSideEffect>() {
+    private val fetchInterestUseCase: FetchInterestUseCase,
+    private val patchSignupInterestUseCase: PatchSignupInterestUseCase,
+    private val stringProvider: StringProvider
+) : BaseStateViewModel<InterestViewModel.InterestTypeUiState, InterestViewModel.InterestTypeSideEffect>() {
 
-    override val _uiStateFlow: MutableStateFlow<InterestUiState> =
-        MutableStateFlow(InterestUiState.Empty)
+    override val _uiStateFlow: MutableStateFlow<InterestTypeUiState> =
+        MutableStateFlow(InterestTypeUiState.LessRequireSelectCount)
 
-    sealed class InterestUiState : UiState {
-        object Empty : InterestUiState()
+    private val _interestMap = MutableStateFlow<Map<Long, InterestModel>>(emptyMap())
+    private val _interestList = MutableStateFlow<List<InterestModel>>(emptyList())
+        .also { flow ->
+            flow.onEach { list ->
+                _interestMap.value = list.groupBy { it.key }
+                    .filter { it.value.isNotEmpty() }
+                    .mapValues { it.value.first() }
+            }.launchIn(viewModelScope)
+        }
+    val interestList = _interestList.asStateFlow()
+
+    private val _selectInterest = MutableStateFlow<Set<InterestModel>>(emptySet())
+        .also { flow ->
+            flow.onEach { set ->
+                _uiStateFlow.value = when (set.size >= MAX_REQUIRE_SELECT_COUNT) {
+                    true -> InterestTypeUiState.FullRequireSelectCount
+                    else -> InterestTypeUiState.LessRequireSelectCount
+                }
+            }.launchIn(viewModelScope)
+        }
+    val selectInterest = _selectInterest.asStateFlow()
+
+    private val _dataLoading = MutableStateFlow(false)
+    val dataLoading = _dataLoading.asStateFlow()
+
+    private var fetchIdealListJob: Job = viewModelScope.launch {
+        _dataLoading.value = true
+        fetchInterestUseCase()
+            .onSuccess {
+                _interestList.value = it.toList()
+            }.onFailure {
+                _uiStateFlow.value = InterestTypeUiState.FetchListFail(
+                    stringProvider.getString(StringProvider.ResId.InterestPatchFail)
+                )
+            }.also {
+                _dataLoading.value = false
+            }
     }
-    sealed class InterestSideEffect : SideEffect
+
+    fun fetchSavedData(phone: String) {
+        viewModelScope.launch {
+            fetchIdealListJob.join()
+            fetchSignupUser(phone)
+        }
+    }
+
+    private suspend fun fetchSignupUser(phone: String) {
+        if (phone.isBlank()) {
+            _uiStateFlow.value = InterestTypeUiState.InvalidPhoneNumber(
+                stringProvider.getString(StringProvider.ResId.InvalidatePhone)
+            )
+            return
+        }
+        _dataLoading.value = true
+        fetchSignupUserUseCase(phone)
+            .onSuccess { user ->
+                val set = _selectInterest.value.toMutableSet()
+                user.interestKeys.forEach { key ->
+                    _interestMap.value[key]?.let { set.add(it) }
+                }
+                if (set.isNotEmpty()) _selectInterest.value = set
+            }.onFailure {
+                _sideEffectFlow.emit(
+                    InterestTypeSideEffect.ShowToast(
+                        stringProvider.getString(StringProvider.ResId.InvalidateSignupProcess) +
+                            stringProvider.getString(StringProvider.ResId.CustomerService)
+                    )
+                )
+            }.also {
+                _dataLoading.value = false
+            }
+    }
+
+    fun idealChipClickEvent(idx: Int) {
+        val selectIdeal = _interestList.value[idx]
+        _selectInterest.value = _selectInterest.value.let {
+            when (it.contains(selectIdeal)) {
+                true -> it.toMutableSet().apply {
+                    remove(selectIdeal)
+                }
+
+                else -> it.toMutableSet().apply {
+                    if (it.size == MAX_REQUIRE_SELECT_COUNT) {
+                        remove(it.iterator().next())
+                    }
+                    add(selectIdeal)
+                }
+            }
+        }
+    }
+
+    fun nextEvent(phone: String) {
+        if (_selectInterest.value.size < MAX_REQUIRE_SELECT_COUNT) {
+            return // show Toast
+        }
+        viewModelScope.launch {
+            _dataLoading.value = true
+            patchSignupInterestUseCase(
+                phone,
+                selectInterest.value.map { it.key }
+            ).onSuccess {
+                _sideEffectFlow.emit(InterestTypeSideEffect.NavigateNextView)
+            }.onFailure {
+                _sideEffectFlow.emit(
+                    InterestTypeSideEffect.ShowToast(
+                        stringProvider.getString(StringProvider.ResId.InterestPatchFail)
+                    )
+                )
+            }.also {
+                _dataLoading.value = false
+            }
+        }
+    }
+
+    sealed class InterestTypeUiState : UiState {
+        data class InvalidPhoneNumber(val message: String) : InterestTypeUiState()
+        data class FetchListFail(val message: String) : InterestTypeUiState()
+        object LessRequireSelectCount : InterestTypeUiState()
+        object FullRequireSelectCount : InterestTypeUiState()
+    }
+
+    sealed class InterestTypeSideEffect : SideEffect {
+        data class ShowToast(val message: String) : InterestTypeSideEffect()
+        object NavigateNextView : InterestTypeSideEffect()
+    }
+
+    companion object {
+        const val MAX_REQUIRE_SELECT_COUNT = 3
+    }
 }
