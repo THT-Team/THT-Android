5b1a3da3f44f4fbb970127f07cbff28f5b146bde
wjchoi96 <wjchoi96@naver.com>
2023-05-17 23:45:03 +0900

    feat:TOP-51 SIgnupRequest에 FcmToken, Sns 관련 데이터 추가

---
+++ data/src/main/java/com/tht/tht/data/remote/request/signup/SignupRequest.kt
@@ -0,0 +1,61 @@
+package com.tht.tht.data.remote.request.signup
+
+
+import com.google.gson.annotations.SerializedName
+
+data class SignupRequest(
+    @SerializedName("agreement")
+    val agreement: Agreement,
+    @SerializedName("birthDay")
+    val birthDay: String,
+    @SerializedName("deviceKey")
+    val deviceKey: String,
+    @SerializedName("email")
+    val email: String,
+    @SerializedName("gender")
+    val gender: String,
+    @SerializedName("idealTypeList")
+    val idealTypeList: List<Long>,
+    @SerializedName("interestList")
+    val interestList: List<Long>,
+    @SerializedName("introduction")
+    val introduction: String,
+    @SerializedName("locationRequest")
+    val locationRequest: LocationRequest,
+    @SerializedName("phoneNumber")
+    val phoneNumber: String,
+    @SerializedName("photoList")
+    val photoList: List<String>,
+    @SerializedName("preferGender")
+    val preferGender: String,
+    @SerializedName("username")
+    val username: String,
+    @SerializedName("deviceKey")
+    val fcmToken: String,
+    @SerializedName("snsType")
+    val snsType: String,
+    @SerializedName("snsUniqueId")
+    val snsUniqueId: String
+) {
+    data class Agreement(
+        @SerializedName("locationServiceAgree")
+        val locationServiceAgree: Boolean,
+        @SerializedName("marketingAgree")
+        val marketingAgree: Boolean,
+        @SerializedName("personalPrivacyInfoAgree")
+        val personalPrivacyInfoAgree: Boolean,
+        @SerializedName("serviceUseAgree")
+        val serviceUseAgree: Boolean
+    )
+
+    data class LocationRequest(
+        @SerializedName("address")
+        val address: String,
+        @SerializedName("lat")
+        val lat: Double,
+        @SerializedName("lon")
+        val lon: Double,
+        @SerializedName("regionCode")
+        val regionCode: Int
+    )
+}
