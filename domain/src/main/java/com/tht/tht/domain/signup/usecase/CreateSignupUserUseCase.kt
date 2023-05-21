8d060a5550925d10d7f5cbc6ec0af1fd7aa39d8a
wjchoi96 <wjchoi96@naver.com>
2023-05-21 21:54:39 +0900

    feat:TOP-52 CreateSignupUserUseCase, RequestPhoneVerifyUseCase에 loginType 매개변수 추가

--- domain/src/main/java/com/tht/tht/domain/signup/usecase/CreateSignupUserUseCase.kt
+++ domain/src/main/java/com/tht/tht/domain/signup/usecase/CreateSignupUserUseCase.kt
@@ -3,6 +3,7 @@ package com.tht.tht.domain.signup.usecase
 import com.tht.tht.domain.signup.model.SignupUserModel
 import com.tht.tht.domain.signup.model.SignupUserModel.Companion.getFromDefaultArgument
 import com.tht.tht.domain.signup.repository.SignupRepository
+import com.tht.tht.domain.type.SignInType
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 
@@ -10,12 +11,15 @@ class CreateSignupUserUseCase(
     private val repository: SignupRepository,
     private val dispatcher: CoroutineDispatcher
 ) {
-
-    internal suspend operator fun invoke(phone: String): SignupUserModel {
+    internal suspend operator fun invoke(
+        phone: String,
+        loginType: SignInType
+    ): SignupUserModel {
         return withContext(dispatcher) {
             repository.saveSignupUser(
                 getFromDefaultArgument(
-                    phone = phone
+                    phone = phone,
+                    snsType = loginType.key
                 )
             )
         }
--- domain/src/main/java/com/tht/tht/domain/signup/usecase/RequestPhoneVerifyUseCase.kt
+++ domain/src/main/java/com/tht/tht/domain/signup/usecase/RequestPhoneVerifyUseCase.kt
@@ -1,6 +1,7 @@
 package com.tht.tht.domain.signup.usecase
 
 import com.tht.tht.domain.signup.repository.SignupRepository
+import com.tht.tht.domain.type.SignInType
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 
@@ -9,12 +10,17 @@ class RequestPhoneVerifyUseCase(
     private val createSignupUserUseCase: CreateSignupUserUseCase,
     private val dispatcher: CoroutineDispatcher
 ) {
-    suspend operator fun invoke(apiAuthNum: String, phone: String, inputAuthNum: String): Result<Boolean> {
+    suspend operator fun invoke(
+        apiAuthNum: String,
+        phone: String,
+        inputAuthNum: String,
+        loginType: SignInType
+    ): Result<Boolean> {
         return kotlin.runCatching {
             withContext(dispatcher) {
                 (apiAuthNum == inputAuthNum).also {
                     if(it && repository.fetchSignupUser(phone) == null) { // 해당 phone 으로 진행 중인 가입 프로세스가 없다면, 새로 생성
-                        createSignupUserUseCase(phone)
+                        createSignupUserUseCase(phone, loginType)
                     }
                 }
             }
