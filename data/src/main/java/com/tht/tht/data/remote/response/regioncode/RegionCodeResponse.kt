52c2715aa1ebff95c169f4fcb2232a9aaa792f5c
kwangddang <kwangddang12@naver.com>
2023-05-24 00:24:45 +0900

    feat: TOP-54 RegionCodeResponse 중첩 클래스로 변경

--- data/src/main/java/com/tht/tht/data/remote/response/regioncode/RegionCodeResponse.kt
+++ data/src/main/java/com/tht/tht/data/remote/response/regioncode/RegionCodeResponse.kt
@@ -5,4 +5,13 @@ import com.google.gson.annotations.SerializedName
 data class RegionCodeResponse(
     @SerializedName("StanReginCd")
     val stanReginCd: List<StanReginCd>
-)
+) {
+    data class StanReginCd(
+        val row: List<Row>?
+    ) {
+        data class Row(
+            @SerializedName("region_cd")
+            val regionCode: String,
+        )
+    }
+}
--- data/src/main/java/com/tht/tht/data/remote/response/regioncode/StanReginCd.kt
+++
@@ -1,5 +0,0 @@
-package com.tht.tht.data.remote.response.regioncode
-
-data class StanReginCd(
-    val row: List<Row>
-)
