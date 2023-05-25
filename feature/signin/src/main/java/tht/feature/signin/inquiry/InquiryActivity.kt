1cad31f5371dc5f58adac634a97db95d8556d172
kwangddang <kwangddang12@naver.com>
2023-05-26 01:56:55 +0900

    feat: TOP-53 InquiryActivity xml 파일 디자인

--- feature/signin/src/main/java/tht/feature/signin/inquiry/InquiryActivity.kt
+++ feature/signin/src/main/java/tht/feature/signin/inquiry/InquiryActivity.kt
@@ -3,6 +3,7 @@ package tht.feature.signin.inquiry
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import tht.core.ui.delegate.viewBinding
+import tht.feature.signin.R
 import tht.feature.signin.databinding.ActivityInquiryBinding
 
 class InquiryActivity : AppCompatActivity() {
@@ -11,5 +12,17 @@ class InquiryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setToolbar()
+    }
+
+    private fun setToolbar() {
+        binding.toolBar.apply {
+            setNavigationIcon(R.drawable.ic_left_arrow)
+            setSupportActionBar(this)
+            setNavigationOnClickListener {
+
+            }
+        }
+        title = null
     }
 }
