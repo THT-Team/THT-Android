589dc56c9199e927e294ada1cf0eeddcd5b08213
wjchoi96 <wjchoi96@naver.com>
2023-05-28 01:37:19 +0900

    feat:TOP-55 ToHotUserInfoCard를 full mode와 minimum mode 컴포넌트로 구분

--- feature/tohot/src/main/java/tht/feature/tohot/component/userinfo/ToHotUserInfoCard.kt
+++ feature/tohot/src/main/java/tht/feature/tohot/component/userinfo/ToHotUserInfoCard.kt
@@ -1,78 +1,80 @@
 package tht.feature.tohot.component.userinfo
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.compose_ui.component.text.headline.ThtHeadline3
-import com.example.compose_ui.component.text.p.ThtP2
-import tht.feature.tohot.R
 import tht.feature.tohot.userData
 
 @Composable
 fun ToHotUserInfoCard(
     modifier: Modifier = Modifier,
+    isFullCardShow: Boolean,
     name: String,
     age: Int,
     address: String,
-    onClick: () -> Unit = { }
+    interests: List<Long>,
+    idealTypes: List<Long>,
+    introduce: String,
+    onClick: (Boolean) -> Unit = { },
+    onReportClick: () -> Unit = { }
 ) {
-    Row(
+    Column(
         modifier = modifier
             .fillMaxWidth()
-            .clickable(
-                enabled = true,
-                onClick = onClick
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioLowBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
             )
     ) {
-        Column(
-            modifier = Modifier.weight(1f)
-        ) {
-            ThtHeadline3(
-                text = "$name, $age",
-                fontWeight = FontWeight.Medium,
-                color = Color(0xFFF9FAFA)
+        if (isFullCardShow) {
+            ToHotUserInfoFullCard(
+                interests = interests,
+                idealTypes = idealTypes,
+                introduce = introduce,
+                onClick = { onClick(true) },
+                onReportClick = onReportClick
             )
-
-            Row(
-                modifier = Modifier.padding(top = 6.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_location),
-                    contentDescription = "location"
-                )
-                ThtP2(
-                    modifier = Modifier.padding(start = 4.dp),
-                    text = address,
-                    fontWeight = FontWeight.Medium,
-                    color = Color(0xFFF9FAFA)
-                )
-            }
         }
-
-        Image(
-            modifier = Modifier.padding(top = 8.dp),
-            painter = painterResource(id = R.drawable.ic_detail),
-            contentDescription = "detail_card"
+        ToHotUserInfoMinimumCard(
+            name = name,
+            age = age,
+            address = address,
+            onClick = { onClick(false) }
         )
     }
 }
 
 @Composable
-@Preview(showBackground = true, backgroundColor = 0xFF000000)
-private fun ToHotUserInfoCardPreview() {
+@Preview(showBackground = true, backgroundColor = 0xFF000000, name = "fullMode")
+private fun FullToHotUserInfoCardPreview() {
     ToHotUserInfoCard(
+        isFullCardShow = true,
         name = userData.nickname,
         age = 32,
-        address = userData.address
+        address = userData.address,
+        interests = userData.interestKeys,
+        idealTypes = userData.idealTypeKeys,
+        introduce = userData.introduce
+    )
+}
+
+@Composable
+@Preview(showBackground = true, backgroundColor = 0xFF000000, name = "minimumMode")
+private fun MinimumToHotUserInfoCardPreview() {
+    ToHotUserInfoCard(
+        isFullCardShow = false,
+        name = userData.nickname,
+        age = 32,
+        address = userData.address,
+        interests = userData.interestKeys,
+        idealTypes = userData.idealTypeKeys,
+        introduce = userData.introduce
     )
 }
---
+++ feature/tohot/src/main/java/tht/feature/tohot/component/userinfo/ToHotUserInfoFullCard.kt
@@ -0,0 +1,143 @@
+package tht.feature.tohot.component.userinfo
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.compose_ui.component.text.p.ThtP2
+import tht.feature.tohot.R
+import tht.feature.tohot.component.chip.ToHotChip
+import tht.feature.tohot.userData
+
+@Composable
+fun ToHotUserInfoFullCard(
+    modifier: Modifier = Modifier,
+    interests: List<Long>,
+    idealTypes: List<Long>,
+    introduce: String,
+    onClick: () -> Unit = { },
+    onReportClick: () -> Unit = { }
+) {
+    Box(
+        modifier = modifier
+        .fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(max = 338.dp)
+                .verticalScroll(rememberScrollState())
+                .clip(RoundedCornerShape(6.dp))
+                .background(
+                    color = Color(0xFF282828).copy(alpha = 0.5f)
+                )
+                .clickable(
+                    enabled = true,
+                    onClick = onClick
+                )
+        ) {
+            ThtP2(
+                modifier = Modifier.padding(start = 12.dp, top = 12.dp),
+                text = stringResource(id = R.string.user_info_interest_title),
+                fontWeight = FontWeight.Medium,
+                color = Color(0xFFF9FAFA)
+            )
+
+            Row(
+                modifier = Modifier
+                    .padding(start = 12.dp, top = 4.dp)
+            ) {
+                interests.forEach { interest ->
+                    ToHotChip(item = interest.toString())
+                    Spacer(modifier = Modifier.width(6.dp))
+                }
+            }
+
+            ThtP2(
+                modifier = Modifier.padding(start = 12.dp, top = 6.dp),
+                text = stringResource(id = R.string.user_info_ideal_type_title),
+                fontWeight = FontWeight.Medium,
+                color = Color(0xFFF9FAFA)
+            )
+
+            Row(
+                modifier = Modifier
+                    .padding(start = 12.dp, top = 4.dp)
+            ) {
+                idealTypes.forEach { ideal ->
+                    ToHotChip(item = ideal.toString())
+                    Spacer(modifier = Modifier.width(6.dp))
+                }
+            }
+
+            ThtP2(
+                modifier = Modifier.padding(start = 12.dp, top = 6.dp),
+                text = stringResource(id = R.string.user_info_introduce_title),
+                fontWeight = FontWeight.Medium,
+                color = Color(0xFFF9FAFA)
+            )
+
+            Box(
+                modifier = Modifier
+                    .padding(start = 12.dp, top = 4.dp, end = 12.dp, bottom = 12.dp)
+                    .clip(RoundedCornerShape(6.dp))
+                    .background(
+                        color = Color(0xFF111111).copy(alpha = 0.5f)
+                    )
+            ) {
+                ThtP2(
+                    modifier = Modifier
+                        .padding(10.dp),
+                    textAlign = TextAlign.Start,
+                    text = introduce,
+                    fontWeight = FontWeight.Medium,
+                    color = Color(0xFFF9FAFA)
+                )
+            }
+        }
+
+        Image(
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(top = 12.dp, end = 12.dp)
+                .clickable(
+                    enabled = true,
+                    onClick = onReportClick
+                ),
+            painter = painterResource(id = R.drawable.ic_report),
+            contentDescription = "report_user"
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+private fun ToHotUserInfoFullCardPreview() {
+    ToHotUserInfoFullCard(
+        interests = userData.interestKeys,
+        idealTypes = userData.idealTypeKeys,
+        introduce = userData.introduce
+    )
+}
