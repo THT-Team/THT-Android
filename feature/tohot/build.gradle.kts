9b6618fb0891827855f5842847f3bdce55e6e637
wjchoi96 <wjchoi96@naver.com>
2023-05-28 00:33:48 +0900

    feat:TOP-55 hilt 종속성 추가

--- feature/tohot/build.gradle.kts
+++ feature/tohot/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -48,6 +50,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso)
+
+    implementation(libs.hilt)
+    testImplementation(libs.hilt.android.testing)
+    kapt(libs.hilt.android.compiler)
+    kaptTest(libs.hilt.android.compiler)
+    kapt(libs.hilt.compiler)
 
     implementation(libs.jetpack.compose.material)
     implementation(libs.jetpack.compose.animation)
