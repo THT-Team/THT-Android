55528194096b84e44b9e5960f54aa3f287bd57ca
wjchoi96 <wjchoi96@naver.com>
2023-05-28 00:42:37 +0900

    add:TOP-55 add compose card component

---
+++ feature/tohot/src/main/java/tht/feature/tohot/component/card/ToHotCard.kt
@@ -0,0 +1,75 @@
+package tht.feature.tohot.component.card
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import tht.feature.tohot.component.pager.ToHotCardImagePager
+import tht.feature.tohot.component.pager.ToHotPagerIndicator
+import tht.feature.tohot.component.progress.ToHotAnimateTimeProgressContainer
+import tht.feature.tohot.component.userinfo.ToHotUserInfoCard
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ToHotCard(
+    modifier: Modifier = Modifier,
+    imageUrls: List<String>,
+    name: String,
+    age: Int,
+    address: String,
+    maxTimeSec: Int,
+    currentSec: Int,
+    ticChanged: (Int) -> Unit = { },
+    userCardClick: () -> Unit
+) {
+    val pagerState = rememberPagerState()
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .clip(RoundedCornerShape(12.dp))
+    ) {
+        ToHotCardImagePager(
+            modifier = Modifier.fillMaxSize(),
+            pagerState = pagerState,
+            imageUrls = imageUrls
+        )
+
+        ToHotAnimateTimeProgressContainer(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(horizontal = 13.dp, vertical = 12.dp),
+            maxTimeSec = maxTimeSec,
+            currentSec = currentSec,
+            ticChanged = ticChanged
+        )
+
+        ToHotUserInfoCard(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .padding(horizontal = 18.dp, vertical = 38.dp),
+            name = name,
+            age = age,
+            address = address,
+            onClick = userCardClick
+        )
+
+        ToHotPagerIndicator(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp, start = 10.dp, end = 10.dp),
+            pagerState = pagerState,
+            pageCount = imageUrls.size,
+            width = 10.dp,
+            height = 10.dp
+        )
+    }
+}
---
+++ feature/tohot/src/main/java/tht/feature/tohot/component/card/ToHotCardImage.kt
@@ -0,0 +1,50 @@
+package tht.feature.tohot.component.card
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.size.Size
+import tht.feature.tohot.R
+import tht.feature.tohot.userData
+
+@Composable
+fun ToHotCardImage(
+    modifier: Modifier = Modifier,
+    imageUrl: String,
+    placeholder: Painter = painterResource(id = R.drawable.ic_location),
+    error: Painter = painterResource(id = R.drawable.ic_location)
+) {
+    val model = ImageRequest.Builder(LocalContext.current)
+        .data(imageUrl)
+        .size(Size.ORIGINAL)
+        .crossfade(true)
+        .build()
+    AsyncImage(
+        modifier = modifier
+            .fillMaxSize()
+            .clip(RoundedCornerShape(12.dp)),
+        model = model,
+        contentDescription = "card_image",
+        placeholder = placeholder,
+        error = error,
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+private fun ToHotCardImagePreview() {
+    ToHotCardImage(
+        imageUrl = userData.profileImgUrl.first()
+    )
+}
