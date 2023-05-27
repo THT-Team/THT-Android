ace4702e6b9ba297f36aabb1418d81b6b1f8d9d5
wjchoi96 <wjchoi96@naver.com>
2023-05-28 01:38:31 +0900

    modify:TOP-55 디버그용 UserData의 introduce 값 변경

--- feature/tohot/src/main/java/tht/feature/tohot/UserData.kt
+++ feature/tohot/src/main/java/tht/feature/tohot/UserData.kt
@@ -19,5 +19,6 @@ internal val userData = SignupUserModel(
         "https://firebasestorage.googleapis.com/v0/b/tht-android-a954a.appspot.com/o/1684673205398_0?alt=media&token=93c6fbbf-20bd-4d43-953c-50c0722c31c2",
         "https://firebasestorage.googleapis.com/v0/b/tht-android-a954a.appspot.com/o/1684673205398_1?alt=media&token=14048744-7aa1-4872-bbc4-aa013f4a6c33"
     ),
-    introduce = "introduce"
+    introduce = "무엇을 위하여서 끝까지 것이다. 새가 위하여서, 싹이 우리의 석가는 위하여 그와 유소년에게서 것이다. 그러므로 불어 방황하였으며, 반짝이는 천하를 동산에는 뿐이다.\n" +
+        "무엇을 위하여서 끝까지 것이다. 새가 위하여서, 싹이 우리의 석가는 위하여 그와 유소년에게서 것이다. 그러므로 불어 방황하였으며, 반짝이는 천하를 동산에는 뿐이다."
 )
