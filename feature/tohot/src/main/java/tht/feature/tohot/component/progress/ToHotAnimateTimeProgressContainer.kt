ca7c2196160710840eab64620cc30e913b1bd181
wjchoi96 <wjchoi96@naver.com>
2023-05-28 23:45:46 +0900

    feat:TOP-55 ToHotAnimateTimeProgressContainer로 progress animation 코드를 끌어올려 ToHotTimeProgressBar, ToHotTimeCircularProgress에서 공통으로 애니메이션을 구현하도록 수정

--- feature/tohot/src/main/java/tht/feature/tohot/component/progress/ToHotAnimateTimeProgressContainer.kt
+++ feature/tohot/src/main/java/tht/feature/tohot/component/progress/ToHotAnimateTimeProgressContainer.kt
@@ -1,6 +1,9 @@
 package tht.feature.tohot.component.progress
 
+import android.util.Log
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
@@ -8,13 +11,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
+/**
+ * 1. maxTime, currentSec 를 받아 ProgressBar 구성
+ * 2. maxTime, destinationSec 로 목표 progress 산출
+ * 3. 애니메이션 수행
+ * 4. 애니메이션 수행 후 ticChanged 호출
+ */
 @Composable
 fun ToHotAnimateTimeProgressContainer(
     modifier: Modifier = Modifier,
@@ -22,19 +33,48 @@ fun ToHotAnimateTimeProgressContainer(
     maxTimeSec: Int,
     currentSec: Int,
     destinationSec: Int,
+    duration: Int = (currentSec - destinationSec) * 1000,
     ticChanged: (Int) -> Unit = { }
 ) {
-    val progress = destinationSec.toFloat() / maxTimeSec.toFloat()
+    val destinationProgress = destinationSec.toFloat() / maxTimeSec.toFloat()
     val progressColor by animateColorAsState(
-        targetValue = if (progress >= 0.7f) {
+        targetValue = if (destinationProgress >= 0.7f) {
             Color(0xFFF9CC2E)
-        } else if (progress >= 0.3f) {
+        } else if (destinationProgress >= 0.3f) {
             Color(0xFFF98F2E)
         } else {
             Color(0xFFF93A2E)
         },
         animationSpec = tween(durationMillis = 1000)
     )
+
+    val progressAnimatable = remember { Animatable((currentSec.toFloat() / maxTimeSec.toFloat())) }
+    LaunchedEffect(key1 = destinationSec, key2 = enable) {
+        if (enable) {
+            Log.d("ToHot", "duration => $duration, progress => ${destinationProgress}, target : ${progressAnimatable.targetValue}, value : ${progressAnimatable.value}")
+            if (progressAnimatable.targetValue == destinationProgress) {
+                ticChanged((progressAnimatable.value * maxTimeSec).toInt())
+                return@LaunchedEffect
+            }
+            if (destinationSec > progressAnimatable.value * maxTimeSec) {
+                progressAnimatable.animateTo(
+                    targetValue = currentSec.toFloat() / maxTimeSec.toFloat(),
+                    animationSpec = tween(
+                        durationMillis = 0,
+                        easing = LinearEasing
+                    )
+                )
+            }
+            progressAnimatable.animateTo(
+                targetValue = destinationProgress,
+                animationSpec = tween(
+                    durationMillis = duration,
+                    easing = LinearEasing
+                )
+            )
+            ticChanged((progressAnimatable.value * maxTimeSec).toInt())
+        }
+    }
 
     Row(
         modifier = modifier
@@ -43,16 +83,21 @@ fun ToHotAnimateTimeProgressContainer(
             .background(color = Color(0xFF1A1A1A).copy(alpha = 0.5f))
             .padding(horizontal = 10.dp, vertical = 4.dp)
     ) {
-        ToHotAnimateTimeLinearProgress(
+        ToHotTimeCircularProgress(
             modifier = Modifier
-                .padding(start = 12.dp)
                 .align(Alignment.CenterVertically),
-            enable = enable,
-            maxTimeSec = maxTimeSec,
-            currentSec = currentSec,
-            destinationSec = destinationSec,
-            ticChanged = ticChanged,
-            color = progressColor
+            size = 24f,
+            color = progressColor,
+            progress = 1 - progressAnimatable.value,
+            duration = duration,
+            sec = currentSec
+        )
+
+        ToHotTimeProgressBar(
+            modifier = Modifier
+                .align(Alignment.CenterVertically),
+            color = progressColor,
+            progress = progressAnimatable.value
         )
     }
 }
