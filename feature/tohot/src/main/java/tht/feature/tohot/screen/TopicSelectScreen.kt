8c6ae75df7040ef5ad45d50b96aa67fb0b237657
wjchoi96 <wjchoi96@naver.com>
2023-06-03 19:32:53 +0900

    chore:#TOP-56 lint check

--- feature/tohot/src/main/java/tht/feature/tohot/screen/TopicSelectScreen.kt
+++ feature/tohot/src/main/java/tht/feature/tohot/screen/TopicSelectScreen.kt
@@ -2,7 +2,6 @@ package tht.feature.tohot.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
