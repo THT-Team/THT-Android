28c99f55896f8cbb8233569c85c95f923a908e4c
wjchoi96 <wjchoi96@naver.com>
2023-06-04 17:51:39 +0900

    feat:TOP-56 ToHotViewModel에 Topic 로직 추가

--- feature/tohot/src/main/java/tht/feature/tohot/viewmodel/ToHotViewModel.kt
+++ feature/tohot/src/main/java/tht/feature/tohot/viewmodel/ToHotViewModel.kt
@@ -2,11 +2,17 @@ package tht.feature.tohot.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.compose_ui.common.viewmodel.Container
 import com.example.compose_ui.common.viewmodel.Store
 import com.example.compose_ui.common.viewmodel.intent
 import com.example.compose_ui.common.viewmodel.store
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import tht.feature.tohot.StringProvider
 import tht.feature.tohot.model.CardTimerUiModel
 import tht.feature.tohot.model.ImmutableListWrapper
@@ -18,8 +24,13 @@ import tht.feature.tohot.userData
 import tht.feature.tohot.userData2
 import tht.feature.tohot.userData3
 import tht.feature.tohot.userData4
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.Stack
 import javax.inject.Inject
+
 
 /**
  * - 손으로 드래그 중에 시간이 다 달면 예외 발생
@@ -38,17 +49,154 @@ class ToHotViewModel @Inject constructor(
     override val store: Store<ToHotState, ToHotSideEffect> =
         store(
             initialState = ToHotState(
-                userList = ImmutableListWrapper(userList),
-                timers = ImmutableListWrapper(
-                    List(userList.size) { CardTimerUiModel(5, 5, 5) }
-                ),
+                loading = false,
+                userList = ImmutableListWrapper(emptyList()),
+                timers = ImmutableListWrapper(emptyList()),
                 enableTimerIdx = 0,
                 selectTopic = null,
-                topicList = ImmutableListWrapper(topics),
-                topicSelectRemainingTime = "24:00:00"
+                topicModalShow = false,
+                topicList = ImmutableListWrapper(emptyList()),
+                topicSelectRemainingTime = "00:00:00",
+                topicSelectRemainingTimeMill = 0
             )
         )
     private var removeUserCardStack = Stack<ToHotUserUiModel>()
+
+    init {
+        toHotLogic()
+    }
+
+    private fun toHotLogic() {
+        with(store.state.value) {
+            when {
+                topicList.list.isEmpty() -> fetchTopicList()
+
+                selectTopic == null  -> {
+                    clearUserCard()
+                    intent {
+                        reduce { it.copy(topicModalShow = true) }
+                    }
+                }
+
+                userList.list.isEmpty() -> requestUserCard(selectTopic.key)
+            }
+        }
+    }
+
+    private fun fetchTopicList() {
+        viewModelScope.launch {
+            intent { reduce { it.copy(loading = true) } }
+            delay(500)
+            intent {
+                reduce {
+                    it.copy(
+                        topicList = ImmutableListWrapper(topics),
+                        topicModalShow = true,
+                        topicSelectRemainingTime = "24:00:00"
+                    )
+                }
+                reduce { it.copy(loading = false) }
+            }
+            toHotLogic()
+        }
+    }
+
+    fun openTopicSelectEvent() {
+        startTopicRemainingTimer()
+    }
+
+    fun closeTopicSelectEvent() {
+        if (::topicRemainingTimer.isInitialized) topicRemainingTimer.cancel()
+    }
+
+    /**
+     * 1 초 마다 LocalDateTime 객체, State 객체를 생성 하는 문제 존재
+     * 미완성
+     */
+    private lateinit var topicRemainingTimer: Job
+    private fun startTopicRemainingTimer() {
+        if (::topicRemainingTimer.isInitialized) topicRemainingTimer.cancel()
+        topicRemainingTimer = viewModelScope.launch(Dispatchers.IO) {
+            with (store.state.value) {
+                while (isActive && topicSelectRemainingTimeMill >= 0) {
+                    delay(1000)
+                    val remainingString = (topicSelectRemainingTimeMill - System.currentTimeMillis()).let {
+                        val date = LocalDateTime.ofInstant(Instant.ofEpochMilli(it), ZoneId.systemDefault())
+                        date.format(DateTimeFormatter.ofPattern("HH:mm:ss"))
+                    }
+                    intent {
+                        reduce {
+                            it.copy(
+                                topicSelectRemainingTime = remainingString,
+                                topicSelectRemainingTimeMill = topicSelectRemainingTimeMill - 1000
+                            )
+                        }
+                    }
+                }
+
+                if (topicSelectRemainingTimeMill < 0) {
+                    fetchTopicList()
+                }
+            }
+        }
+    }
+
+    private fun clearUserCard() {
+        intent {
+            reduce {
+                it.copy(
+                    userList = ImmutableListWrapper(emptyList()),
+                    timers = ImmutableListWrapper(emptyList())
+                )
+            }
+        }
+    }
+
+    private fun requestUserCard(topicKey: Long) {
+        viewModelScope.launch {
+            intent { reduce { it.copy(loading = true) } }
+            delay(500)
+            intent {
+                reduce {
+                    it.copy(
+                        userList = ImmutableListWrapper(userList),
+                        timers = ImmutableListWrapper(
+                            List(userList.size) {
+                                CardTimerUiModel(MAX_TIMER_SEC, MAX_TIMER_SEC, MAX_TIMER_SEC)
+                            }
+                        )
+                    )
+                }
+                reduce { it.copy(loading = false) }
+            }
+        }
+    }
+
+    fun topicSelectEvent(topicKey: Long) {
+        intent {
+            reduce {
+                it.copy(
+                    selectTopic = it.topicList.list.firstOrNull { t -> t.key == topicKey }
+                )
+            }
+        }
+    }
+
+    fun topicSelectFinishEvent() {
+        viewModelScope.launch {
+            intent { reduce { it.copy(loading = true) } }
+            delay(500)
+            intent {
+                reduce {
+                    it.copy(
+                        topicModalShow = false
+                    )
+                }
+                reduce { it.copy(loading = false) }
+            }
+            toHotLogic()
+        }
+    }
 
     fun userChangeEvent(userIdx: Int) {
         Log.d("ToHot", "userChangeEvent => $userIdx")
@@ -59,9 +207,9 @@ class ToHotViewModel @Inject constructor(
                     timers = ImmutableListWrapper(
                         it.timers.list.toMutableList().apply {
                             this[userIdx] = this[userIdx].copy(
-                                maxSec = 5,
-                                currentSec = 5,
-                                destinationSec = 4
+                                maxSec = MAX_TIMER_SEC,
+                                currentSec = MAX_TIMER_SEC,
+                                destinationSec = MAX_TIMER_SEC - 1
                             )
                         }
                     ),
@@ -129,5 +277,9 @@ class ToHotViewModel @Inject constructor(
                 )
             }
         }
+    }
+
+    companion object {
+        private const val MAX_TIMER_SEC = 5
     }
 }
