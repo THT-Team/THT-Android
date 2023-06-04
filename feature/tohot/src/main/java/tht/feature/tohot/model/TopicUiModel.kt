6860c7d9a17075a81132c20afbca11636758af16
wjchoi96 <wjchoi96@naver.com>
2023-06-04 23:20:36 +0900

    feat:TOP-36 TopicUiModel에 icon 정보 추가
     - emoji를 사용하지 않고 icon url, resource를 사용할 계획이라 추가
     - 추후 emoji 제거 예정

--- feature/tohot/src/main/java/tht/feature/tohot/model/TopicUiModel.kt
+++ feature/tohot/src/main/java/tht/feature/tohot/model/TopicUiModel.kt
@@ -1,5 +1,6 @@
 package tht.feature.tohot.model
 
+import tht.feature.tohot.R
 import javax.annotation.concurrent.Immutable
 
 @Immutable
@@ -7,7 +8,9 @@ data class TopicUiModel(
     val emoji: String,
     val key: Long,
     val title: String,
-    val content: String
+    val content: String,
+    val iconUrl: String,
+    val iconRes: Int
 )
 
 val topics = listOf(
@@ -15,18 +18,24 @@ val topics = listOf(
         emoji = "1F3AE",
         title = "게임",
         key = 1,
-        content = "게임 좋아하세요?"
+        content = "게임 좋아하세요?",
+        iconUrl = "123",
+        iconRes = R.drawable.ic_topic_value_38
     ),
     TopicUiModel(
         emoji = "1F4DA",
         title = "독서",
         key = 2,
-        content = "책읽을래요??"
+        content = "책읽을래요??",
+        iconUrl = "123",
+        iconRes = R.drawable.ic_topic_rest_38
     ),
     TopicUiModel(
         emoji = "1F3AC",
         title = "영화/드라마",
         key = 3,
-        content = "영화 뭐좋아해요?"
+        content = "영화 뭐좋아해요?",
+        iconUrl = "123",
+        iconRes = R.drawable.ic_topic_value_38
     )
 )
