ec05d18c70b07889b4a543d74a49bf45b6a7434f
kwangddang <kwangddang12@naver.com>
2023-06-14 03:34:10 +0900

    feat: TOP-59 LikeDetailFragment 임시 디자인

---
+++ feature/like/src/main/java/tht/feature/like/detail/LikeDetailFragment.kt
@@ -0,0 +1,27 @@
+package tht.feature.like.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
+import tht.core.ui.delegate.viewBinding
+import tht.feature.heart.databinding.DialogDetailBinding
+
+@AndroidEntryPoint
+class LikeDetailFragment : BottomSheetDialogFragment() {
+
+    private val binding by viewBinding(DialogDetailBinding::inflate)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
