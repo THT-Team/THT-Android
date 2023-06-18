ba0e37b5b585857afe3319d310483758f11c049f
kwangddang <kwangddang12@naver.com>
2023-06-18 22:27:30 +0900

    feat: TOP-59 AlertDialog core-ui로 변경

--- feature/like/src/main/java/tht/feature/like/detail/LikeDetailFragment.kt
+++ feature/like/src/main/java/tht/feature/like/detail/LikeDetailFragment.kt
@@ -1,21 +1,14 @@
 package tht.feature.like.detail
 
-import android.app.AlertDialog
 import android.app.Dialog
-import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
-import android.view.WindowMetrics
-import android.widget.ArrayAdapter
 import android.widget.ImageView
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.chip.Chip
@@ -23,14 +16,14 @@ import com.google.android.material.chip.ChipGroup
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import tht.core.ui.delegate.viewBinding
-import tht.core.ui.extension.getPxFromDp
+import tht.core.ui.dialog.setBottomSheetDialogSize
+import tht.core.ui.dialog.showCustomAlertDialog
 import tht.core.ui.extension.repeatOnStarted
 import tht.feature.heart.R
 import tht.feature.heart.databinding.DialogDetailBinding
 import tht.feature.like.constant.LikeConstant
 import tht.feature.like.like.LikeModel
 import tht.feature.like.like.LikeViewModel
-import kotlin.math.roundToInt
 
 @AndroidEntryPoint
 class LikeDetailFragment : BottomSheetDialogFragment() {
@@ -49,7 +42,7 @@ class LikeDetailFragment : BottomSheetDialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog: Dialog = super.onCreateDialog(savedInstanceState)
         dialog.setOnShowListener {
-            setDialogSize(it as BottomSheetDialog)
+            setBottomSheetDialogSize(requireActivity(), it as BottomSheetDialog)
         }
         return dialog
     }
@@ -134,65 +127,17 @@ class LikeDetailFragment : BottomSheetDialogFragment() {
     }
 
     private fun showReportDialog() {
-        val menus = arrayOf(
-            requireContext().getString(R.string.menu_report),
-            requireContext().getString(R.string.menu_block)
+        showCustomAlertDialog(
+            requireContext(),
+            requireActivity(),
+            arrayOf(
+                requireContext().getString(R.string.menu_report),
+                requireContext().getString(R.string.menu_block),
+            ),
+            R.layout.item_dialog_subt2_semi_bold,
+            true
         )
-
-        val dialog = AlertDialog.Builder(requireContext(), R.style.ReportDialogStyle)
-            .setAdapter(
-                ArrayAdapter(
-                    requireContext(),
-                    R.layout.item_dialog_subt2_text,
-                    menus
-                )
-            ) { dialog, which ->
-                when (which) {
-                    0 -> viewModel.reportEvent(likeUser.nickname)
-                    1 -> viewModel.blockEvent(likeUser.nickname)
-                }
-                dialog.dismiss()
-            }
-            .create()
-
-        dialog.setOnShowListener {
-            dialog.window?.setLayout(
-                getWindowSize().second * 85 / 100,
-                WindowManager.LayoutParams.WRAP_CONTENT
-            )
-        }
-
-        dialog.apply {
-            listView.divider = ColorDrawable(resources.getColor(tht.core.ui.R.color.gray_666666, null))
-            listView.dividerHeight = requireContext().getPxFromDp(1.5f).roundToInt()
-            listView.setFooterDividersEnabled(false)
-            listView.addFooterView(View(requireContext()))
-        }.show()
     }
-
-    private fun setDialogSize(bottomSheetDialog: BottomSheetDialog) {
-        val bottomSheet =
-            bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as View
-        val behavior = BottomSheetBehavior.from<View>(bottomSheet)
-        val layoutParams = bottomSheet.layoutParams
-        val height = getWindowSize().first * 85 / 100
-        layoutParams.height = height
-        bottomSheet.layoutParams = layoutParams
-        behavior.state = BottomSheetBehavior.STATE_EXPANDED
-        behavior.peekHeight = height
-    }
-
-    private fun getWindowSize(): Pair<Int, Int> =
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val windowMetrics: WindowMetrics = requireActivity().windowManager.currentWindowMetrics
-            val bounds = windowMetrics.bounds
-            bounds.height() to bounds.width()
-        } else {
-            val displayMetrics = DisplayMetrics()
-            val display = requireActivity().windowManager.defaultDisplay
-            display.getRealMetrics(displayMetrics)
-            displayMetrics.heightPixels to displayMetrics.widthPixels
-        }
 
     private fun loadImage(view: ImageView, url: String) {
         Glide.with(view)
