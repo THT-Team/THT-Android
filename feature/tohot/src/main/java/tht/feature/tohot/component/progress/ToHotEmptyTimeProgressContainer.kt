4e122050665a36cf5072a2dc38a184a57f75c743
wjchoi96 <wjchoi96@naver.com>
2023-06-19 23:50:51 +0900

    add:TOP-55 ToHotEmptyTimerProgressContainer 추가

---
+++ feature/tohot/src/main/java/tht/feature/tohot/component/progress/ToHotEmptyTimeProgressContainer.kt
@@ -0,0 +1,55 @@
+package tht.feature.tohot.component.progress
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import tht.feature.tohot.R
+
+@Composable
+fun ToHotEmptyTimeProgressContainer(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(10.dp))
+            .background(color = Color(0xFF1A1A1A).copy(alpha = 0.5f))
+            .padding(horizontal = 10.dp)
+    ) {
+        Image(
+            modifier = Modifier
+                .size(36.dp)
+                .align(Alignment.CenterVertically)
+                .padding(end = 12.dp),
+            painter = painterResource(id = R.drawable.ic_timer_empty),
+            contentDescription = "empty_timer"
+        )
+
+        ToHotTimeProgressBar(
+            modifier = Modifier
+                .align(Alignment.CenterVertically),
+            color = colorResource(id = tht.core.ui.R.color.black_222222),
+            progress = 0f
+        )
+    }
+}
+
+@Composable
+@Preview
+fun ToHotEmptyTimeProgressContainerPreview() {
+    ToHotEmptyTimeProgressContainer()
+}
+
