1ab0dde15f737cdb566023cfa767c96596c6135f
wjchoi96 <wjchoi96@naver.com>
2023-06-24 17:59:56 +0900

    chore:TOP-55 lint check

--- feature/tohot/src/main/java/tht/feature/tohot/component/progress/ToHotEmptyTimeProgressContainer.kt
+++ feature/tohot/src/main/java/tht/feature/tohot/component/progress/ToHotEmptyTimeProgressContainer.kt
@@ -1,17 +1,10 @@
 package tht.feature.tohot.component.progress
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
