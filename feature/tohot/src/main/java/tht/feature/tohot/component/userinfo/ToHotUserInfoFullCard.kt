cd419aa23f34956508980b7d130b0ee17e599061
wjchoi96 <wjchoi96@naver.com>
2023-07-14 00:40:39 +0900

    design:TOP-58 ToHotUserInfoFullCard의 report 버튼 아이콘의 배경 제거
     - 아이콘 변경
     - 자연스러운 ripple 효과를 위해 IconButton으로 변경

--- feature/tohot/src/main/java/tht/feature/tohot/component/userinfo/ToHotUserInfoFullCard.kt
+++ feature/tohot/src/main/java/tht/feature/tohot/component/userinfo/ToHotUserInfoFullCard.kt
@@ -1,6 +1,5 @@
 package tht.feature.tohot.component.userinfo
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -14,11 +13,14 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -125,17 +127,18 @@ fun ToHotUserInfoFullCard(
             }
         }
 
-        Image(
+        IconButton(
             modifier = Modifier
                 .align(Alignment.TopEnd)
-                .padding(top = 12.dp, end = 12.dp)
-                .clickable(
-                    enabled = true,
-                    onClick = onReportClick
-                ),
-            painter = painterResource(id = R.drawable.ic_report),
-            contentDescription = "report_user"
-        )
+                .padding(top = 12.dp, end = 12.dp),
+            onClick = onReportClick
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_report),
+                contentDescription = "report_user",
+                tint = colorResource(id = tht.core.ui.R.color.white_f9fafa)
+            )
+        }
     }
 }
 
