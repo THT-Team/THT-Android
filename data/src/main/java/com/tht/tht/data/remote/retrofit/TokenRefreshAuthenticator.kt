d7251a61176eda6223f7893ba2e7f7e47c8885a5
wjchoi96 <wjchoi96@naver.com>
2023-07-16 22:00:45 +0900

    feat:TOP-66 TokenRefreshAuthenticator에 @Inject constructor를 추가하고, Provide 코드를 작성

--- data/src/main/java/com/tht/tht/data/remote/retrofit/TokenRefreshAuthenticator.kt
+++ data/src/main/java/com/tht/tht/data/remote/retrofit/TokenRefreshAuthenticator.kt
@@ -7,9 +7,10 @@ import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
 import okhttp3.Route
+import javax.inject.Inject
 
 
-class TokenRefreshAuthenticator(
+class TokenRefreshAuthenticator @Inject constructor(
     private val refreshFcmTokenLoginUseCase: Lazy<RefreshFcmTokenLoginUseCase>
 ) : Authenticator {
 
