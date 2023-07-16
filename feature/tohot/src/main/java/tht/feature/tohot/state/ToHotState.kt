58932aa1c13b51a95164d00acf89a229f6c34672
wjchoi96 <wjchoi96@naver.com>
2023-07-17 00:01:33 +0900

    chore:TOP-58 ToHotState에 주석 추가
     - state의 속성이 너무 많아 이해도를 위해 주석 추가

--- feature/tohot/src/main/java/tht/feature/tohot/state/ToHotState.kt
+++ feature/tohot/src/main/java/tht/feature/tohot/state/ToHotState.kt
@@ -13,9 +13,9 @@ data class ToHotState(
     val userList: ImmutableListWrapper<ToHotUserUiModel>,
     val userCardState: ToHotCardState, // Empty 일 경우 보여줄 View 를 정함 -> TODO: enum 으로 관리 해서 실패 처리도 포함?
     val timers: ImmutableListWrapper<CardTimerUiModel>,
-    val enableTimerIdx: Int,
-    val fallingAnimationIdx: Int = -1,
-    val cardMoveAllow: Boolean,
+    val enableTimerIdx: Int, // 현재 표시 되는 Card Idx -> 해당 Card 의 Timer 진행됨
+    val fallingAnimationIdx: Int = -1, // 신고, 차단 Animation Idx
+    val cardMoveAllow: Boolean, // card suspend 기능. false 일 경우 Timer 중단. Dialog 등이 표시 될 때 사용
     val reportMenuDialogShow: Boolean = false,
     val reportDialogShow: Boolean = false,
     val blockDialogShow: Boolean = false,
