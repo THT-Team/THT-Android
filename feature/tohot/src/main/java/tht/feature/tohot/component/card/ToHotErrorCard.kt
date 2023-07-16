cf0922be11285c73a30b01e768c92bc5b297a38e
wjchoi96 <wjchoi96@naver.com>
2023-07-16 23:51:45 +0900

    add:TOP-58 Add ToHotErrorCard

---
+++ feature/tohot/src/main/java/tht/feature/tohot/component/card/ToHotErrorCard.kt
@@ -0,0 +1,27 @@
+package tht.feature.tohot.component.card
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import tht.feature.tohot.R
+
+@Composable
+fun ToHotErrorCard(
+    modifier: Modifier = Modifier
+) {
+    ToHotNotifyCard(
+        modifier = modifier,
+        image = painterResource(id = R.drawable.ic_mudy_error),
+        title = stringResource(id = R.string.to_hot_error_card_title),
+        description = stringResource(id = R.string.to_hot_error_card_description),
+        buttonText = stringResource(id = R.string.retry)
+    )
+}
+
+@Composable
+@Preview
+private fun ToHotErrorCardPreview() {
+    ToHotErrorCard()
+}
