30f94b56022efaa74ad36cdd8165f46c76fba5c6
wjchoi96 <wjchoi96@naver.com>
2023-09-06 22:12:14 +0900

    fix:TOP-71 Timer Animation 중단 후 재개할 경우, progress 는 줄어 들어 있는 상태 에서 duration 은 기존 값대로 유지 되어 Animation 이 빠르게 진행 되는 문제 해결

--- feature/tohot/src/main/java/tht/feature/tohot/component/progress/ToHotAnimateTimeProgressContainer.kt
+++ feature/tohot/src/main/java/tht/feature/tohot/component/progress/ToHotAnimateTimeProgressContainer.kt
@@ -43,7 +43,7 @@ fun ToHotAnimateTimeProgressContainer(
     duration: Float = ((currentSec - destinationSec) * 1000),
     ticChanged: (Float) -> Unit = { }
 ) {
-    Log.d("ToHot", "Timer => c[$currentSec], d[$destinationSec]")
+    Log.d("Timer", "cSec[$currentSec], dSec[$destinationSec]")
     val destinationProgress = destinationSec / maxTimeSec.toFloat()
     var color = progressColor.lastOrNull() ?: Color.Yellow
     for (i in progressColor.indices) {
@@ -55,38 +55,61 @@ fun ToHotAnimateTimeProgressContainer(
     }
     val animateProgressColor by animateColorAsState(
         targetValue = color,
-        animationSpec = tween(durationMillis = 1000),
+        animationSpec = tween(durationMillis = duration.toInt()),
         label = "animateProgressColor"
     )
 
     val progressAnimatable = remember { Animatable((currentSec / maxTimeSec.toFloat())) }
     LaunchedEffect(key1 = destinationSec, key2 = enable) {
         if (enable) {
-            Log.d(
-                "ToHot",
-                "duration => $duration, progress => $destinationProgress," +
-                    " target : ${progressAnimatable.targetValue}, value : ${progressAnimatable.value}"
-            )
             if (progressAnimatable.targetValue == destinationProgress) {
-                ticChanged((progressAnimatable.value * maxTimeSec))
-                return@LaunchedEffect
-            }
-            if (destinationSec > progressAnimatable.value * maxTimeSec) {
+                /**
+                 * Timer Animation 중단 후 재개할 경우, progress 는 줄어 들어 있는 상태 에서
+                 * duration 은 기존 값대로 유지 되어 Animation 이 빠르게 진행 되는 문제
+                 * -> 현재 progress 에 걸맞는 새로운 duration 계산
+                 *
+                 * 1. 한 Tic 동안 줄어 들어야 하는 progress 계산
+                 * - 총 5초일 경우 1.0 / 5 -> 0.2
+                 *
+                 * 2. 한 Tic 동안 돌아야 하는 잔여 progress 계산
+                 * - progress 가 0.85 에서 중단 된 후 재개 된다면 현재 Tic 에서 0.5 progress 진행 필요
+                 * - (0.85 - (destinationSec[4] * oneTicProgress[0.25]))
+                 * -> destinationSec[4] * oneTicProgress[0.25]) 값은, destinationSec 에 도달 했을 progress[0.8] 을 의미
+                 *
+                 * 3. 잔여 progress 를 oneTicProgress 에 곱한 후 본래 duration 에 곱해서 잔여 duration 계산
+                 * - 0.05 / 0.2 => 0.25
+                 * - 0.25 * 1000 => 250
+                 */
+                val oneTicProgress = 1 / maxTimeSec.toFloat() // 한 틱 동안 줄어 들어야 하는 progress.value
+                val oneTicRemainingProgress = progressAnimatable.value - (destinationSec * oneTicProgress)
+                val remainingDuration = oneTicRemainingProgress / oneTicProgress * duration
+                Log.d(
+                    "Timer remaining",
+                    "progressAnimatable.value[${progressAnimatable.value}], " +
+                        " oneTicRemainingProgress[$oneTicRemainingProgress], " +
+                        "remainingDuration[$remainingDuration]"
+                )
                 progressAnimatable.animateTo(
-                    targetValue = currentSec / maxTimeSec.toFloat(),
+                    targetValue = destinationProgress,
                     animationSpec = tween(
-                        durationMillis = 0,
+                        durationMillis = remainingDuration.toInt(),
+                        easing = LinearEasing
+                    )
+                )
+            } else {
+                Log.d(
+                    "Timer",
+                    "duration => $duration, progress => $destinationProgress," +
+                        " target : ${progressAnimatable.targetValue}, value : ${progressAnimatable.value}"
+                )
+                progressAnimatable.animateTo(
+                    targetValue = destinationProgress,
+                    animationSpec = tween(
+                        durationMillis = duration.toInt(),
                         easing = LinearEasing
                     )
                 )
             }
-            progressAnimatable.animateTo(
-                targetValue = destinationProgress,
-                animationSpec = tween(
-                    durationMillis = duration.toInt(),
-                    easing = LinearEasing
-                )
-            )
             ticChanged((progressAnimatable.value * maxTimeSec))
         }
     }
