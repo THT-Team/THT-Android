b6e9bf85f172f4839a3bcc4960348abc2d906b49
wjchoi96 <wjchoi96@naver.com>
2024-01-21 20:54:45 +0900

    refactor:TOP-93 TermsDao를 TermsService로 변경
     - Remote, Local 변경 가능성을 두고 Service로 변경

--- data/src/main/java/com/tht/tht/data/di/DaoModule.kt
+++ data/src/main/java/com/tht/tht/data/di/DaoModule.kt
@@ -1,11 +1,9 @@
 package com.tht.tht.data.di
 
-import com.tht.tht.data.local.dao.TokenDao
-import com.tht.tht.data.local.dao.TokenDaoImpl
 import com.tht.tht.data.local.dao.SignupUserDao
 import com.tht.tht.data.local.dao.SignupUserDaoImpl
-import com.tht.tht.data.local.dao.TermsDao
-import com.tht.tht.data.local.dao.TermsDaoImpl
+import com.tht.tht.data.local.dao.TokenDao
+import com.tht.tht.data.local.dao.TokenDaoImpl
 import com.tht.tht.data.local.dao.topic.DailyTopicDao
 import com.tht.tht.data.local.dao.topic.DailyTopicDaoImpl
 import dagger.Binds
@@ -17,10 +15,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DaoModule {
-
-    @Binds
-    @Singleton
-    abstract fun bindTermsDao(impl: TermsDaoImpl): TermsDao
 
     @Binds
     @Singleton
--- data/src/main/java/com/tht/tht/data/di/ServiceModule.kt
+++ data/src/main/java/com/tht/tht/data/di/ServiceModule.kt
@@ -1,24 +1,27 @@
 package com.tht.tht.data.di
 
-import com.tht.tht.data.remote.service.location.LocationService
-import com.tht.tht.data.remote.service.location.LocationServiceImpl
+import com.tht.tht.data.local.service.TermsService
+import com.tht.tht.data.local.service.TermsServiceImpl
 import com.tht.tht.data.remote.service.email.EmailService
 import com.tht.tht.data.remote.service.email.EmailServiceImpl
+import com.tht.tht.data.remote.service.location.LocationService
+import com.tht.tht.data.remote.service.location.LocationServiceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class ServiceModule {
 
     @Binds
-    @Singleton
     abstract fun bindLocationService(impl: LocationServiceImpl): LocationService
 
     @Binds
-    @Singleton
     abstract fun bindEmailService(impl: EmailServiceImpl): EmailService
+
+    @Binds
+    abstract fun bindTermsService(impl: TermsServiceImpl): TermsService
+
 }
