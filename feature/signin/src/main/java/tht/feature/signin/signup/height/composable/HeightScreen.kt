11b7d57118b8d3b5f6d6e562c79d8363e76ac339
wjchoi96 <wjchoi96@naver.com>
2024-04-27 16:17:07 +0900

    feat:TOP-96 SignupSmallButton Composable 구현

--- feature/signin/src/main/java/tht/feature/signin/signup/height/composable/HeightScreen.kt
+++ feature/signin/src/main/java/tht/feature/signin/signup/height/composable/HeightScreen.kt
@@ -1,24 +1,16 @@
 package tht.feature.signin.signup.height.composable
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -27,21 +19,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_ui.component.progress.ThtCircularProgress
 import com.example.compose_ui.component.text.headline.ThtHeadline1
-import com.example.compose_ui.component.text.p.ThtP2
 import com.example.compose_ui.extensions.noRippleClickable
 import tht.core.ui.R
 import tht.feature.signin.signup.height.HeightUiState
 import tht.feature.signin.ui.SignupDescription
+import tht.feature.signin.ui.SignupSmallButton
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -155,29 +144,13 @@ private fun HeightScreenContent(
         )
 
         Spacer(modifier = Modifier.weight(1f))
-        Button(
+        SignupSmallButton(
             modifier = Modifier
-                .width(88.dp)
-                .height(54.dp)
                 .imePadding()
                 .align(Alignment.End),
-            shape = RoundedCornerShape(16.dp),
-            enabled = height != null,
-            onClick = onNext,
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = colorResource(id = R.color.yellow_f9cc2e),
-                contentColor = Color.Transparent,
-                disabledBackgroundColor = colorResource(id = R.color.brown_26241f),
-                disabledContentColor = Color.Transparent
-            ),
-            contentPadding = PaddingValues(vertical = 16.dp)
-        ) {
-            Icon(
-                painter = painterResource(id = tht.feature.signin.R.drawable.ic_right_arrow_black),
-                contentDescription = "ic_right_arrow_black",
-                tint = Color.Black
-            )
-        }
+            enable = height != null,
+            onClick = onNext
+        )
         Spacer(modifier = Modifier.height(42.dp))
     }
 }
