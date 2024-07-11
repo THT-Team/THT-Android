5efab17e61cb09d79a8544d45c39ebf0e693793b
taewooyo <skaxodn97@gmail.com>
2024-07-11 19:52:20 +0900

    chore:TOP-96 compose-ui module activity 버전 1.7.1 -> 1.9.0

--- core/compose-ui/build.gradle.kts
+++ core/compose-ui/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
 
-    implementation("androidx.activity:activity-compose:1.7.1")
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.material:material:1.4.3")
     implementation("androidx.compose.animation:animation:1.4.3")
     implementation("androidx.compose.ui:ui-tooling:1.4.3")
