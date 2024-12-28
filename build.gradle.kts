3e7bf49076ef69c5594682946112589acf9231e7
wjchoi96 <wjchoi96@naver.com>
2024-12-28 18:22:32 +0900

    feat:TOP-108 AGP, Compose, kotlin, targetSdkVersion 등 버전 최신화
     - JDK 17변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 buildscript {
     extra.apply {
-        set("compileSdk", 34)
+        set("compileSdk", 35)
         set("minSdkVersion", 26)
-        set("targetSdk", 34)
+        set("targetSdk", 35)
         set("versionCode", 1)
         set("versionName", "1.0")
     }
